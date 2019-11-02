87cf5d4b215b8aee3c16edd057083a3480b6734e
1010creative <54685439+1010creative@users.noreply.github.com>
2019-11-03 02:32:16 +0700

    Update beentogether.ads
    update config

--- beentogether.ads
+++ beentogether.ads
@@ -1,16 +1,11 @@
 {
   "android": {
+    "ad_network":1,
     "admob_banner_id": "ca-app-pub-2313206220567592/9450342169",
     "admob_banner_setting_id": "ca-app-pub-2313206220567592/5728473593",
     "admob_interstitial_id": "ca-app-pub-2313206220567592/1620664045",
     "admob_reward_id": "ca-app-pub-2313206220567592/6616340390",
-    "adx_banner_id": "/21617015150/52210046/21810981620",
-    "adx_banner_setting_id": "/21617015150/52210046/21811129618",
-    "adx_interstitial_id": "/21617015150/52210046/21811126384",
-    "adx_reward_id": "/21617015150/52210046/21811126450",
-    "use_banner_adx": 0,
-    "use_interstitial_adx": 0,
-    "use_reward_adx": 0,
+    "ir_src_app_key":"9975875d",
     "time_wait_splash":5000,
     "interval_show_ad":60000,
     "enable_rate":true,
@@ -19,18 +14,6 @@
     "enable_remove_ad_promote":true
   },
   "ios": {
-    "admob_banner_id": "admob_banner_id",
-    "admob_banner_setting_id": "admob_banner_setting_id",
-    "admob_interstitial_id": "admob_interstitial_id",
-    "admob_reward_id": "admob_reward_id",
-    "adx_banner_id": "adx_banner_id",
-    "adx_banner_setting_id": "adx_banner_setting_id",
-    "adx_interstitial_id": "adx_interstitial_id",
-    "adx_reward_id": "adx_reward_id",
-    "use_banner_adx": 0,
-    "use_interstitial_adx": 0,
-    "use_reward_adx": 0,
-    "time_wait_splash":6000,
-    "interval_show_ad":30000
+    
   }
 }
