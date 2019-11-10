a570d61df11904b923055f8838a9873d858c5021
1010creative <54685439+1010creative@users.noreply.github.com>
2019-11-10 15:53:33 +0700

    Update kbc2017.ads
    fix

--- kbc2017.ads
+++ kbc2017.ads
@@ -1,5 +1,5 @@
 {
-  "ad_network":1
+  "ad_network":1,
   "admob_banner_ios_id": "admob_banner_ios_id",
   "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
   "admob_video_reward_ios_id": "admob_video_reward_ios_id",
