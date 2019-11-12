8220886fdacd0d6ca29b287c2797ed5798ee6be9
1010creative <54685439+1010creative@users.noreply.github.com>
2019-11-12 23:07:26 +0700

    Update kbc2017.ads
    update fan

--- kbc2017.ads
+++ kbc2017.ads
@@ -1,9 +1,9 @@
 {
   "ad_network":0,
-  "admob_banner_ios_id": "admob_banner_ios_id",
-  "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
-  "admob_video_reward_ios_id": "admob_video_reward_ios_id",
   "admob_banner_android_id": "ca-app-pub-2313206220567592/1817857557",
   "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/5251682016",
-  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/4603064301"
+  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/4603064301",
+  "fan_banner_android":"807991949620374_807999529619616",
+  "fan_full_android":"807991949620374_808741652878737",
+  "fan_video_android":"807991949620374_808742442878658"
 }
