ba1bc9c07f29e1b7387716c18f2b1c4b2bbc8cd8
1010creative <54685439+1010creative@users.noreply.github.com>
2019-11-14 00:48:40 +0700

    Update hindikbc2017.ads
    update fan

--- hindikbc2017.ads
+++ hindikbc2017.ads
@@ -1,8 +1,11 @@
 {
-  "admob_banner_ios_id": "admob_banner_ios_id",
-  "admob_fullscreen_ios_id": "admob_fullscreen_ios_id",
-  "admob_video_reward_ios_id": "admob_video_reward_ios_id",
+  "banner_ad_network":0,
+  "interstitial_ad_network":1,
+  "video_ad_network":0,
   "admob_banner_android_id": "ca-app-pub-2313206220567592/5779432061",
   "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/1840187057",
-  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/8325047815"
+  "admob_video_reward_android_id": "ca-app-pub-2313206220567592/8325047815",
+  "fan_banner_android":"",
+  "fan_full_android":"490102331716906_490105668383239",
+  "fan_video_android":""
 }
