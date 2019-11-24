8d6911bb51e108b839e0bb489226e2a4d701400a
1010creative <54685439+1010creative@users.noreply.github.com>
2019-11-24 22:06:45 +0700

    Update blockpuzzle.ads

--- blockpuzzle.ads
+++ blockpuzzle.ads
@@ -8,7 +8,7 @@
   "admob_banner_android_id": "ca-app-pub-2313206220567592/9182471068",
   "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/1659204260",
   "admob_video_reward_android_id": "ca-app-pub-2313206220567592/5565876355",
-  "fan_banner_android":"",
-  "fan_full_android":"",
-  "fan_video_android":""
+  "fan_banner_android":"521929205053916_521935961719907",
+  "fan_full_android":"521929205053916_521937201719783",
+  "fan_video_android":"521929205053916_521937481719755"
 }
