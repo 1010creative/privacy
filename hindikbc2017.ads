93ee3e8cb79da1a0eadd8d0b5ffc5b0976df343a
1010creative <54685439+1010creative@users.noreply.github.com>
2019-12-02 21:48:13 +0700

    Update hindikbc2017.ads

--- hindikbc2017.ads
+++ hindikbc2017.ads
@@ -1,7 +1,7 @@
 {
   "banner_ad_network":0,
-  "interstitial_ad_network":0,
-  "video_ad_network":0,
+  "interstitial_ad_network":1,
+  "video_ad_network":1,
   "admob_banner_android_id": "ca-app-pub-2313206220567592/5779432061",
   "admob_fullscreen_android_id": "ca-app-pub-2313206220567592/1840187057",
   "admob_video_reward_android_id": "ca-app-pub-2313206220567592/8325047815",
